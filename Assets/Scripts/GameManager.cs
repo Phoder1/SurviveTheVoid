60ce33c4884b5b63bcaed47cd71eb7167412455a
ronchabarr <ronchabarr@gmail.com>
2020-12-22 18:33:58 +0200

    FirstPrototype inputmanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      
+        //Debug.Log("hi");
     }
 }
---
+++ Assets/Scripts/States/IdleState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu()]
+public class IdleState : StateBase
+{
+    public int yotamosmamos;
+    public override void OnUpdate()
+    {
+        Debug.Log("Idle");
+        base.OnUpdate();
+
+    }
+}
+
+
+
+
