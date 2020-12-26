c5ba914be406c8b822f7aabe42a82039d1e44a58
Phoder1 <alon.talmi@gmail.com>
2020-12-26 15:10:23 +0200

    Recipe makeover

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Inventory
 {
-    static List<ResourceStruct> inventoryList;
+    static List<ResourceSO> inventoryList;
     const int maxCapacityPerSlot = 40;
     const int maxCapacityOfItems = 25;
 
@@ -13,19 +13,19 @@ public class Inventory
     {
         if (inventoryList == null)
         {
-            inventoryList = new List<ResourceStruct>();
+            inventoryList = new List<ResourceSO>();
         }
     }
 
-    public static List<ResourceStruct> GetInventory {get => inventoryList;}
+    public static List<ResourceSO> GetInventory {get => inventoryList;}
 
-    public static void AddToInventory(ResourceStruct item)
+    public static void AddToInventory(ResourceSO item)
     {
     
     }
-    public static void RemoveObjectFromInventtory(ResourceStruct item, int amount) { }
+    public static void RemoveObjectFromInventtory(ResourceSO item, int amount) { }
 
-    public static bool HasObjectInInventory(ResourceStruct item, int amount) {
+    public static bool HasObjectInInventory(ResourceSO item, int amount) {
         return true;
     }
 }
