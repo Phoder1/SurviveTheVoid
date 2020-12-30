fd9c739790ff53442f34a4bf5b752c3c4f5d0baf
elor504 <74613768+elor504@users.noreply.github.com>
2020-12-30 22:30:43 +0200

    s

--- Assets/Scripts/ScriptableObjects/Scripts/ItemSO.cs
+++ Assets/Scripts/ScriptableObjects/Scripts/ItemSO.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public enum ResourceType
+public enum ItemName
 {
     Flower,
     OakLog,
@@ -19,23 +19,53 @@ public enum ResourceType
 
 
 
-[CreateAssetMenu(fileName = "New Resource", menuName = "Crafting/" + "Resource")]
+public enum ItemType
+{
+    None,
+
+    Consumable,
+    Building,
+    Equipable
+
+}
+
+public enum EquipableType
+{
+    None,
+
+    Helmet,
+    Chest,
+    HandOne,
+    HandTwo,
+    Legging,
+
+    Sword,
+    Axe
+}
+
+
+
+
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Crafting/" + "Item")]
 public class ItemSO : ScriptableObject
 {
     [SerializeField]
     private int maxStackSize;
-    public int getmaxStackSize { get => maxStackSize; }
+    public int getmaxStackSize => maxStackSize; 
 
     [SerializeField]
     private string description;
-    public string getDescription { get => description; }
+    public string getDescription => description; 
 
     [SerializeField]
-    private ResourceType itemEnum;
-    public ResourceType getItemEnum { get => itemEnum; }
+    private ItemName itemEnum;
+    public ItemName getItemEnum => itemEnum; 
 
     [SerializeField]
     private Sprite sprite;
     public Sprite getsprite { get => sprite; }
 
 }
+
+
---
+++ Assets/Scripts/ScriptableObjects/Scripts/ToolItemSO.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item Tool", menuName = "Crafting/" + "Tool")]
+public class ToolItemSO : ItemSO
+{
+    [SerializeField]
+    EquipableType equipType;
+
+    [SerializeField]
+    private float gatheringSpeed;
+    public float getGatheringSpeed => gatheringSpeed;
+
+    [SerializeField]
+    private int durability;
+    public int getDurability => durability;
+
+    // int tier
+    [SerializeField]
+    private int toolTier;
+    public int getToolTier => toolTier;
+
+}
