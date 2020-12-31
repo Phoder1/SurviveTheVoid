49738794f709aaad777d1f6f938bc84e0c80b973
Rei <reisegev@yahoo.com>
2020-12-31 12:44:52 +0200

    Fixed Inventory references in crafting manager

--- Assets/Scripts/Managers/Inventory/Inventory.cs
+++ Assets/Scripts/Managers/Inventory/Inventory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class Inventory : IInventory
+public class Inventory 
 {
 
     private static Inventory _instance;
