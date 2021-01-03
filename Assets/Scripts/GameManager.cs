78140faa2cea1e8edc2b231a026dd55aef1a4179
Phoder1 <alon.talmi@gmail.com>
2021-01-03 13:44:01 +0200

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoSingleton<GameManager>
 {
     public static GameManager _instance;
 
