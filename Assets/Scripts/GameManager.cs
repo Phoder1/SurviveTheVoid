6c9ac00dd296fcb17ab6990fb53928b02001f347
Phoder1 <alon.talmi@gmail.com>
2021-01-03 13:44:34 +0200

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,21 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoSingleton<GameManager>
 {
-    public static GameManager _instance;
-
     GridManager gridManager;
     PlayerManager playerManager;
     CameraScript cameraScript;
     CraftingManager craftingManager;
-
-    private void Awake() {
-        if (_instance != null) {
-            Destroy(gameObject);
-        }
-        else {
-            _instance = this;
-        }
-    }
     // Start is called before the first frame update
     void Start()
     {
