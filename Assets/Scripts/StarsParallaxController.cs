3a2686be9be62d32fc202a969feb7cf92c04aa06
Phoder1 <alon.talmi@gmail.com>
2021-01-09 11:52:39 +0200

    New tiles and stars background zoom fix

---
+++ Assets/Scripts/StarsParallaxController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class StarsParallaxController : MonoBehaviour
+{
+    //private Vector2 cameraRealSize => new Vector2(mainCamera.orthographicSize * 2 * mainCamera.aspect, mainCamera.orthographicSize * 2);
+    private Camera mainCamera;
+    private float cameraSize;
+
+    [SerializeField] private StarsParallax[] starsParallax;
+
+    private void Start() {
+        mainCamera = Camera.main;
+        cameraSize = mainCamera.orthographicSize;
+    }
+
+
+    private void Update() {
+        foreach (StarsParallax stars in starsParallax) {
+            stars.offset += new Vector2(stars.speed * Time.deltaTime, 0f);
+
+            stars.mr.sharedMaterial.SetTextureOffset("_MainTex", stars.offset);
+        }
+    }
+
+    public void UpdateViewSize() {
+        foreach (StarsParallax stars in starsParallax) {
+            stars.mr.transform.localScale *= mainCamera.orthographicSize / cameraSize;
+        }
+        cameraSize = mainCamera.orthographicSize;
+    }
+
+
+    [System.Serializable]
+    private class StarsParallax
+    {
+        [SerializeField] internal MeshRenderer mr;
+        [Range(0.001f, 0.01f)]
+        [SerializeField] internal float speed;
+        internal Vector2 offset = Vector2.zero;
+    }
+}
