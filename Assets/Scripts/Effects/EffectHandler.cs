e746dbb3d86c8a6a7331befe1795884e8cef41f2
Phoder1 <alon.talmi@gmail.com>
2021-01-22 20:47:22 +0200

    EffectHandler + ConsumablesHandler

--- Assets/Scripts/Effects/EffectHandler.cs
+++ Assets/Scripts/Effects/EffectHandler.cs
@@ -7,42 +7,18 @@ public class EffectHandler : MonoSingleton<EffectHandler>
 {
 
     PlayerStats playerStats;
-    static Dictionary<StatType, StatControllers> ConsumablesEffectsDict;
+    
     [SerializeField] float cooldownBeforeStart;
     public override void Init() {
         playerStats = PlayerStats._instance;
 
-        FillDictionary();
+        
 
         StopCoroutine(SurvivalEffects());
         StartCoroutine(SurvivalEffects());
     }
 
-    private void FillDictionary() {
-        ConsumablesEffectsDict = new Dictionary<StatType, StatControllers>();
 
-        AddToDict(StatType.HP, 3, 3);
-        AddToDict(StatType.MaxHP, 3, 3);
-        AddToDict(StatType.Food, 3, 3);
-        AddToDict(StatType.MaxFood, 3, 3);
-        AddToDict(StatType.Water, 3, 3);
-        AddToDict(StatType.MaxWater, 3, 3);
-        AddToDict(StatType.Air, 3, 3);
-        AddToDict(StatType.MaxAir, 3, 3);
-        AddToDict(StatType.Sleep, 3, 3);
-        AddToDict(StatType.MaxSleep, 3, 3);
-        AddToDict(StatType.Temperature, 3, 3);
-        AddToDict(StatType.Level, 3, 3);
-        AddToDict(StatType.EXP, 3, 3);
-        AddToDict(StatType.EXPtoNextLevel, 3, 3);
-        AddToDict(StatType.MoveSpeed, 3, 3);
-        AddToDict(StatType.AttackDMG, 3, 3);
-        AddToDict(StatType.GatheringSpeed, 3, 3);
-    }
-    private void AddToDict(StatType statType, float cooldown, float overtimeCooldown) {
-        Stat stat = playerStats.GetStat(statType);
-        ConsumablesEffectsDict.Add(statType, new StatControllers(new EffectController(stat, cooldown), new EffectController(stat, overtimeCooldown)));
-    }
 
 
     IEnumerator SurvivalEffects() {
@@ -78,23 +54,6 @@ public class EffectHandler : MonoSingleton<EffectHandler>
         worldEffect.Begin(sleepEffect);
 
     }
-
-
-    public static EffectController GetStatController(EffectData effect) {
-        if (ConsumablesEffectsDict.TryGetValue(effect.effectStatType, out StatControllers effectControllers))
-            return effectControllers.GetController(effect.effectType);
-        return null;
-    }
-    public static EffectController[] GetStatControllers(EffectData[] effectsData) {
-        if (effectsData.Length == 0)
-            return null;
-        EffectController[] controllers = new EffectController[effectsData.Length];
-        for (int i = 0; i < effectsData.Length; i++)
-            controllers[i] = GetStatController(effectsData[i]);
-        return controllers;
-    }
-    public void BeginAllConsumeableEffects(EffectData[] effectsData)
-        => BeginAllEffects(effectsData, GetStatControllers(effectsData));
     public void BeginAllEffects(EffectData[] effectsData, EffectController[] effectController) {
         for (int i = 0; i < effectsData.Length; i++)
             if (effectsData[i] != null)
@@ -113,54 +72,7 @@ public class EffectHandler : MonoSingleton<EffectHandler>
             controllers[i] = new EffectController(playerStats.GetStat(effectsData[i].effectStatType), coolDowns[i]);
         return controllers;
     }
-    public bool GetEffectCoolDown(ConsumableItemSO Item) {
-
-        if (Item == null || Item.Effects.Length == 0)
-            return false;
-
-        EffectController abstStat;
-
-        bool CanUseTheItem = true;
-        foreach (var effect in Item.Effects) {
-
-            abstStat = GetStatController(effect);
-
-            if (abstStat == null)
-                continue;
-
-
-            CanUseTheItem = CanUseTheItem && !abstStat.isOnCoolDown;
-
-
-            if (CanUseTheItem == false)
-                return false;
-
-        }
-
-        return CanUseTheItem;
-
-    }
 }
-public class StatControllers
-{
-    public readonly EffectController valueController;
-    public readonly EffectController regenerationController;
 
-    public StatControllers(EffectController valueController, EffectController overTimeController) {
-        this.valueController = valueController;
-        this.regenerationController = overTimeController;
-    }
-    public EffectController GetController(EffectType effectType) {
-        switch (effectType) {
-            case EffectType.Instant:
-            case EffectType.Toggle:
-                return valueController;
-            case EffectType.OverTime:
-                return regenerationController;
-            default:
-                return null;
-        }
-    }
-}
 
 
