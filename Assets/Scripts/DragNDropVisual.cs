bdea317771384dff435455a64631e50f256302d3
JinMoshchenko <jinmoshchenko@gmail.com>
2021-01-23 14:55:13 +0200

    fixed

--- Assets/Scripts/DragNDropVisual.cs
+++ Assets/Scripts/DragNDropVisual.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DragNDropVisual : MonoBehaviour
+public class DragNDropVisual : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private RectTransform itemImageTransform;
+
+
+	private void Awake()
+	{
+		itemImageTransform = GetComponent<RectTransform>();
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		Debug.Log("Clicked");
+	}
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		Debug.Log("Drag begin");
+	}
+	public void OnDrag(PointerEventData eventData)
+	{
+		Debug.Log("On drag");
+
+		// Getting movement delta (the amount of units that the finger moved since the previous frame)
+		itemImageTransform.anchoredPosition += eventData.delta;
+	}
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		Debug.Log("Drag end");
+	}
 }
