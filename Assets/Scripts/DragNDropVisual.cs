04bf0a9b9de501c59113b6cda72772b98bb3ce7f
JinMoshchenko <jinmoshchenko@gmail.com>
2021-01-23 16:15:36 +0200

    gh

--- Assets/Scripts/DragNDropVisual.cs
+++ Assets/Scripts/DragNDropVisual.cs
@@ -1,19 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragNDropVisual : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+public class DragNDropVisual : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
 	[SerializeField]
 	private Canvas mainCanvas;
-
 	private RectTransform itemImageTransform;
+	private CanvasGroup itemCanvasGroup;
+	private Vector2 startPosition;
+	[SerializeField]
+	private Transform parent;
+	[SerializeField]
+	private Transform dragParent;
 
+	public bool IsDraggable;
 
 	private void Awake()
 	{
 		itemImageTransform = GetComponent<RectTransform>();
+		itemCanvasGroup = GetComponent<CanvasGroup>();
+
+		startPosition = itemImageTransform.position;
 	}
 
 
@@ -23,11 +30,13 @@ public class DragNDropVisual : MonoBehaviour, IPointerDownHandler, IBeginDragHan
 	}
 	public void OnBeginDrag(PointerEventData eventData)
 	{
-		Debug.Log("Drag begin");
-	}
-	public void OnEndDrag(PointerEventData eventData)
-	{
-		Debug.Log("Drag end");
+		if (IsDraggable == true)
+		{
+			Debug.Log("Drag begin");
+			itemCanvasGroup.alpha = 0.6f;
+			itemCanvasGroup.blocksRaycasts = false;
+			transform.SetParent(dragParent);
+		}
 	}
 	public void OnDrag(PointerEventData eventData)
 	{
@@ -36,8 +45,14 @@ public class DragNDropVisual : MonoBehaviour, IPointerDownHandler, IBeginDragHan
 		// Getting movement delta (the amount of units that the finger moved since the previous frame)
 		itemImageTransform.anchoredPosition += eventData.delta / mainCanvas.scaleFactor;
 	}
-	public void OnDrop(PointerEventData eventData)
+	public void OnEndDrag(PointerEventData eventData)
 	{
-		Debug.Log("On drop");
+		Debug.Log("Drag end");
+
+		itemCanvasGroup.alpha = 1f;
+		itemCanvasGroup.blocksRaycasts = true;
+		itemImageTransform.position = startPosition;
+		transform.SetParent(parent);
+		transform.SetSiblingIndex(0);
 	}
 }
