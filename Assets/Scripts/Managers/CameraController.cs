77a771c249b121f07315800b0700d45d4a579c5b
Phoder1 <alon.talmi@gmail.com>
2021-01-23 23:41:52 +0200

    Player movement fix

--- Assets/Scripts/Managers/CameraController.cs
+++ Assets/Scripts/Managers/CameraController.cs
@@ -9,7 +9,7 @@ public class CameraController : MonoSingleton<CameraController>
     private Camera zoomedInCamera;
 
     private GridManager gridManager;
-    private Vector2 GetCameraRealSize => new Vector2(GetCurrentActiveCamera.orthographicSize * 2 * GetCurrentActiveCamera.aspect, GetCurrentActiveCamera.orthographicSize * 2);
+    private Vector2 GetCameraRealSize => new Vector2(zoomedOutCamera.orthographicSize * 2 * zoomedOutCamera.aspect, zoomedOutCamera.orthographicSize * 2);
     private Vector3 GetCamCornerPosition => new Vector3(transform.position.x, transform.position.y, 0) - (Vector3)GetCameraRealSize / 2;
     private Rect GetWorldView => new Rect(_instance.GetCamCornerPosition, _instance.GetCameraRealSize);
 
@@ -44,9 +44,9 @@ public class CameraController : MonoSingleton<CameraController>
 
         zoomedOutCameraObj.SetActive(!isZoomIn);
         zoomedInCameraObj.SetActive(isZoomIn);
+        Camera previousCamera = GetCurrentActiveCamera;
         currentActiveCamera = (isZoomIn ? zoomedInCamera : zoomedOutCamera);
-        //starsCont.UpdateViewSize();
-        UpdateView();
+        //starsCont.UpdateViewSize(previousCamera.orthographicSize, GetCurrentActiveCamera.orthographicSize);
     }
     public void UpdateView() {
         gridManager.UpdateView(GetWorldView);
