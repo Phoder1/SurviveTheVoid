1475772c482841a23062da546c15b80b1bb3d8d0
JinMoshchenko <jinmoshchenko@gmail.com>
2021-01-23 15:14:02 +0200

    itemdropslot

--- Assets/Scripts/DragNDropVisual.cs
+++ Assets/Scripts/DragNDropVisual.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragNDropVisual : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+public class DragNDropVisual : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
 {
 	[SerializeField]
 	private Canvas mainCanvas;
@@ -16,6 +16,7 @@ public class DragNDropVisual : MonoBehaviour, IPointerDownHandler, IBeginDragHan
 		itemImageTransform = GetComponent<RectTransform>();
 	}
 
+
 	public void OnPointerDown(PointerEventData eventData)
 	{
 		Debug.Log("Clicked");
@@ -24,6 +25,10 @@ public class DragNDropVisual : MonoBehaviour, IPointerDownHandler, IBeginDragHan
 	{
 		Debug.Log("Drag begin");
 	}
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		Debug.Log("Drag end");
+	}
 	public void OnDrag(PointerEventData eventData)
 	{
 		Debug.Log("On drag");
@@ -31,8 +36,8 @@ public class DragNDropVisual : MonoBehaviour, IPointerDownHandler, IBeginDragHan
 		// Getting movement delta (the amount of units that the finger moved since the previous frame)
 		itemImageTransform.anchoredPosition += eventData.delta / mainCanvas.scaleFactor;
 	}
-	public void OnEndDrag(PointerEventData eventData)
+	public void OnDrop(PointerEventData eventData)
 	{
-		Debug.Log("Drag end");
+		Debug.Log("On drop");
 	}
 }
