f256739a74d0687d1948c1b2f080793a25ce5a9f
ronchabarr <ronchabarr@gmail.com>
2021-01-25 17:32:00 +0200

    MainMenu

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField]Canvas SettingMenu;
+    public void playButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void SettingButton()
+    {
+        SettingMenu.enabled = true;
+    }
+    public void CloseSettings()
+    {
+        SettingMenu.enabled = false;
+    }
+}
